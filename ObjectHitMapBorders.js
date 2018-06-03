3818bd9ff7020fba1fd26eee970cf525da762926
Borislav Kalinski <borislav_kalinski@abv.bg>
2018-06-03 21:05:57 +0300

    Update ObjectHitMapBorders.js

--- ObjectHitMapBorders.js
+++ ObjectHitMapBorders.js
@@ -4,66 +4,81 @@ function PlatformTouchMapBorder()
 	{
 		Platform.direction = "rigth";
 	}
-	else if(Platform.Y + Platform.sizeY == map.width)//rigth border
+	else if(Platform.Y + Platform.sizeY == map.width - informationFieldSize)//rigth border
 	{
 		Platform.direction = "left";
 	}
 }
 
-function BallTouchPlatform()
+function DropTouchBottomBorder()
 {
-	if(Ball.X + Ball.radius / 2 == Platform.X &&  Ball.Y >= Platform.Y + Math.round(Platform.sizeY / 3) && Ball.Y <= Platform.Y + Platform.sizeY - Math.round(Platform.sizeY / 3)) // middle part of the platform
+	if(Drop.X == map.height - 50 - Drop.size)
 	{
-		Ball.direction = "up";
-	}
-	if(Ball.X + Ball.radius / 2 == Platform.X &&  Ball.Y >= Platform.Y + 1 && Ball.Y <= Platform.Y + Math.round(Platform.sizeY / 3) + 1) // left part of the platform
-	{
-		Ball.direction = "upLeft";
-	}
-	if(Ball.X + Ball.radius / 2 == Platform.X &&  Ball.Y <= Platform.Y + Platform.sizeY + 1 && Ball.Y >= Platform.Y + Platform.sizeY - Math.round(Platform.sizeY / 3) + 1) // rigth  part of the platform
-	{
-		Ball.direction = "upRigth";
+		isDropFalling = true;
+		Drop.X = 700;
 	}
 }
 
-function BallTouchMapBorder()
+function BallTouchMapBorder(i)
 {
-	if(Ball.X - Ball.radius / 2 == 0 &&  Ball.Y > 0 && Ball.Y < map.width && Ball.direction == "up") // top border
+	if(Ball.position[i].x - Ball.radius / 2 == 0 &&  Ball.position[i].y > 0 && Ball.position[i].y < map.width) // top border
 	{
-		Ball.direction = "down";
-	}
-	else if(Ball.X - Ball.radius / 2 == 0 &&  Ball.Y > 0 && Ball.Y < map.width && Ball.direction == "upLeft") // top border
-	{
-		Ball.direction = "downLeft";
-	}
-	else if(Ball.X - Ball.radius / 2 == 0 &&  Ball.Y > 0 && Ball.Y < map.width && Ball.direction == "upRigth") // top border
-	{
-		Ball.direction = "downRigth";
+		switch(Ball.position[i].direction)
+		{
+			case "up":
+				Ball.position[i].direction = "down";
+				break;
+			case "upLeft":
+				Ball.position[i].direction = "downLeft";
+				break;
+			case "upRigth":
+				Ball.position[i].direction = "downRigth";
+				break;
+		}
 	}
 	//-------------------------------------------------------------------------------------------------------------------------------
-	else if(Ball.X + Ball.radius / 2 == map.height &&  Ball.Y > 0 && Ball.Y < map.width) // bottom border
+	else if(Ball.position[i].x + Ball.radius / 2 == map.height - 50 &&  Ball.position[i].y > 0 && Ball.position[i].y < map.width) // bottom border
 	{
-		context.clearRect(0, 0, map.width, map.height);
-		console.log("gameOver");
-		Ball.Y = map.width / 2;
-		Ball.X = map.height - 100;
+		if(Platform.lives == 0)
+		{	
+			isGameOver = true;
+		}
+		if(Platform.lives > 0 && Ball.position.length == 1)
+		{
+			Ball.position[0].y = map.width / 2 - 100;
+			Ball.position[0].x = map.height - 200;
+			Ball.position[0].direction = "down";
+			Platform.lives--;
+		}
+		else if(Ball.position.length > 1)
+		{
+			Ball.position.splice(i, 1);
+		}
 	}
 	//---------------------------------------------------------------------------------------------------------------------------------
-	else if(Ball.X > 0 && Ball.X < map.height && Ball.Y == 0 && Ball.direction == "upLeft") // left border
+	else if(Ball.position[i].x > 0 && Ball.position[i].x < map.height && Ball.position[i].y == 0) // left border
 	{
-		Ball.direction = "upRigth";
-	}
-	else if(Ball.X > 0 && Ball.X < map.height && Ball.Y == 0 && Ball.direction == "downLeft") // left border
-	{
-		Ball.direction = "downRigth";
+		switch(Ball.position[i].direction)
+		{
+			case "upLeft":
+				Ball.position[i].direction = "upRigth";
+				break;
+			case "downLeft":
+				Ball.position[i].direction = "downRigth";
+				break;
+		}	
 	}
 	//---------------------------------------------------------------------------------------------------------------------------------
-	else if(Ball.X > 0 && Ball.X < map.height && Ball.Y == map.width &&  Ball.direction == "upRigth") // rigth border
+	else if(Ball.position[i].x > 0 && Ball.position[i].x < map.height && Ball.position[i].y == map.width - informationFieldSize) // rigth border
 	{
-		Ball.direction = "upLeft";
-	}
-	else if(Ball.X > 0 && Ball.X < map.height && Ball.Y == map.width &&  Ball.direction == "downRigth") // rigth border
-	{
-		Ball.direction = "downLeft";
+		switch(Ball.position[i].direction)
+		{
+			case "upRigth":
+				Ball.position[i].direction = "upLeft";
+				break;
+			case "downRigth":
+				Ball.position[i].direction = "downLeft";
+				break;
+		}	
 	}
 }
