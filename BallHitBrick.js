5aabad0ba1b3f2e313c217a3431b76d79fd536aa
Borislav Kalinski <borislav_kalinski@abv.bg>
2018-06-03 21:02:47 +0300

    Update BallHitBrick.js

--- BallHitBrick.js
+++ BallHitBrick.js
@@ -1,85 +1,98 @@
 function CheckForFallingDrop(i) 
 {
-	if(fallDrop == true) // check if the drop is fall
+	if(isDropFalling == true) // check if the drop is fall
 	{
 		Drop.X = Brick.position[i].x;
 		Drop.Y = Brick.position[i].y - Brick.sizeY;
-		collorIndex = Math.round(Math.random() * 3);
-		drawDrop = true;
+		collorIndex = 7;/*Math.round(Math.random() * 7);*/
 		Drop.direction = "down";
-		fallDrop = false;
+		isDropFalling = false;
+		isDropDrawed = true;
 	}
 }
 
-function RemoveBrick(i, direction)
+function RemoveBrick(i, direction, j)
 {
 	score++;
-	Ball.direction = direction;
-	Brick.position.splice(i, 1);
-	CheckForFallingDrop(i);
+	Ball.position[j].direction = direction;
+	if(Brick.position[i].colorIndex == 0)
+	{	
+		Brick.position.splice(i, 1);
+		CheckForFallingDrop(i);
+	}
+	else
+	{
+		Brick.position[i].colorIndex--;
+	}
 }
 
-function BallTouchBrick()
+function BallTouchBrick(j)
 {
 	for(var i = 0; i < Brick.position.length; i++)
 	{
-		//-------------------------------------------------------------bottom side of brick------------------------------------------------------
-		if(Ball.X - Ball.radius / 2 == Brick.position[i].x + Brick.sizeX &&  Ball.Y >= Brick.position[i].y && Ball.Y <= Brick.position[i].y + Brick.sizeY + 1)
+		if(Ball.position[j].y >= Brick.position[i].y && Ball.position[j].y <= Brick.position[i].y + Brick.sizeY + 1)
 		{
-			switch(Ball.direction)
+//-------------------------------------------------------------bottom side of brick------------------------------------------------------
+			if(Ball.position[j].x - Ball.radius / 2 == Brick.position[i].x + Brick.sizeX)
 			{
-				case "up":
-					RemoveBrick(i, "down");
-					break;
-				case "upLeft":
-					RemoveBrick(i, "downLeft");
-					break;
-				case "upRigth":
-					RemoveBrick(i, "downRigth");
-					break;
+				switch(Ball.position[j].direction)
+				{
+					case "up":
+						RemoveBrick(i, "down", j);
+						break;
+					case "upLeft":
+						RemoveBrick(i, "downLeft", j);
+						break;
+					case "upRigth":
+						RemoveBrick(i, "downRigth", j);
+						break;
+				}
+			}
+//-------------------------------------------------------------top side of brick--------------------------------------------------------
+			if(Ball.position[j].x + Ball.radius / 2 == Brick.position[i].x)
+			{
+				RemoveBrick(i, "up", j);
+				switch(Ball.position[j].direction)
+				{
+					case "down":
+						RemoveBrick(i, "up", j);
+						break;
+					case "downLeft":
+						RemoveBrick(i, "upLeft", j);
+						break;
+					case "downRigth":
+						RemoveBrick(i, "upRigth", j);
+						break;
+				}
 			}
 		}
-		//-------------------------------------------------------------top side of brick--------------------------------------------------------
-		if(Ball.X + Ball.radius / 2 == Brick.position[i].x &&  Ball.Y >= Brick.position[i].y && Ball.Y <= Brick.position[i].y + Brick.sizeY + 1)
+		if(Ball.position[j].x >= Brick.position[i].x && Ball.position[j].x <= Brick.position[i].x + Brick.sizeX + 1)
 		{
-			RemoveBrick(i, "up");
-			switch(Ball.direction)
+//------------------------------------------------------------left side of brick--------------------------------------------------------------
+			if(Ball.position[j].y+ Ball.radius / 2 == Brick.position[i].y)
 			{
-				case "down":
-					RemoveBrick(i, "up");
-					break;
-				case "downLeft":
-					RemoveBrick(i, "upLeft");
-					break;
-				case "downRigth":
-					RemoveBrick(i, "upRigth");
-					break;
+				switch(Ball.position[j].direction)
+				{
+					case "upRigth":
+						RemoveBrick(i, "upLeft", j);
+						break;
+					case "downRigth":
+						RemoveBrick(i, "downLeft", j);
+						break;
+				}
 			}
-		}
-		//------------------------------------------------------------left side of brick--------------------------------------------------------------
-		if(Ball.Y + Ball.radius / 2 == Brick.position[i].y && Ball.X >= Brick.position[i].x && Ball.X <= Brick.position[i].x + Brick.sizeX + 1)
-		{
-			switch(Ball.direction)
+//-----------------------------------------------------------rigth side of brick----------------------------------------------------------------
+			if(Ball.position[j].y - Ball.radius / 2 == Brick.position[i].y  + Brick.sizeY)
 			{
-				case "upRigth":
-					RemoveBrick(i, "upLeft");
-					break;
-				case "downRigth":
-					RemoveBrick(i, "downLeft");
-					break;
-			}
-		}
-		//-----------------------------------------------------------rigth side of brick----------------------------------------------------------------
-		if(Ball.Y - Ball.radius / 2 == Brick.position[i].y  + Brick.sizeY && Ball.X >= Brick.position[i].x && Ball.X <= Brick.position[i].x + Brick.sizeX + 1)
-		{
-			switch(Ball.direction)
-			{
-				case "upLeft":
-					RemoveBrick(i, "upRigth");
-					break;
-				case "downLeft":
-					RemoveBrick(i, "downRigth");
-					break;
+				switch(Ball.position[j].direction)
+				{
+					case "upLeft":
+						RemoveBrick(i, "upRigth", j);
+						break;
+					case "downLeft":
+						RemoveBrick(i, "downRigth", j);
+						break;
+				}
 			}
 		}
 	}
